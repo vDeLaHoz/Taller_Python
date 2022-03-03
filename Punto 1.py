8c75eb263cc20750bee5bd39fec949b47aa37f3a
Victor De La Hoz <victordelahoz13@gmail.com>
2022-03-02 19:30:21 -0500

    Punto 1

--- Punto 1.py
+++ Punto 1.py
@@ -5,3 +5,19 @@ Created on Wed Mar  2 19:10:47 2022
 @author: vdelahoz6
 """
 
+donacion = float(input('Ingrese el valor de la donacion: $'))
+
+tel = donacion*0.1
+
+print(f'El valor correspondiente a telecomunicaciones es: ${tel:,}')
+
+sistemas = donacion*0.25
+print(f'El valor correspondiente a sistemas es: ${sistemas:,}')
+
+administracion = donacion*0.35
+print(f'El valor correspondiente a administracion es: ${administracion:,}')
+
+contabilidad = donacion*0.3
+print(f'El valor correspondiente a contabilidad es: ${contabilidad:,}')
+
+ 
